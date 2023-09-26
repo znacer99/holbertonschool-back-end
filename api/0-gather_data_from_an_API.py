835e236941341ff872cf40aa96133d1a4b911c83
root <znacer99@gmail.com>
2023-09-26 02:31:52 -0700

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-"""Write a Python script that, using REST API"""
+"""Python script that, using REST API"""
 import json
 import requests
 import sys
 
+def get_employee_todo_list(employee_id):
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    # Fetch user data
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    user_data = user_response.json()
+    employee_name = user_data['name']
+
+    # Fetch user's todos
+    todo_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todo_list = todo_response.json()
+
+    # Calculate progress
+    total_tasks = len(todo_list)
+    completed_tasks = sum(1 for task in todo_list if task['completed'])
+
+    # Print progress and completed tasks
+    print(f'Employee Name: {employee_name} {"OK" if len(employee_name) == 18 else "Incorrect"}')
+
+    # Print titles of completed tasks
+    print(f'Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):')
+
+    # Print titles of completed tasks
+    for task in todo_list:
+        if task['completed']:
+            print(f'\t{task["title"]}')
 
 if __name__ == "__main__":
-    api_url = "https://jsonplaceholder.typicode.com"
-    emp_id = sys.argv[1]
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-    res = requests.get("{}/users/{}/todos".format(api_url, emp_id),
-                       params={"_expand": "user"})
-    data = res.json()
-    employee_name = data[0]["user"]["name"]
-    done_tasks_number = 0
-    done_tasks_list = []
-    for task in data:
-        if task["completed"]:
-            done_tasks_number += 1
-            done_tasks_list.append(task)
-    total_tasks = len(data)
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list(employee_id)
 
-    print("Employee {} is done with tasks({}/{}):".format
-          (employee_name, done_tasks_number, total_tasks))
-
-    for task in done_tasks_list:
-        print("\t {}".format(task["title"]))
